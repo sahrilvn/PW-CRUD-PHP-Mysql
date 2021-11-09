8b146b6bade8727962d7ca44cd5301225e1bae9b
Sahril Angga Permana <43839821+sahrilvn@users.noreply.github.com>
2021-11-09 19:04:12 +0700

    Update proses_hapus.php

--- proses_hapus.php
+++ proses_hapus.php
@@ -1,7 +1,7 @@
 <?php 
 include '+koneksi.php';
 $id = $_GET['id'];
-$query = mysql_query("DELETE FROM kendaraan WHERE id = '$id'") or die(mysql_error());
+$query = mysql_query("DELETE FROM laut WHERE id = '$id'") or die(mysql_error());
 if($query) {
     echo "<script>alert('Data berhasil dihapus!'); window.location='index.php';</script>";
 } else {
