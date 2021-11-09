48417b830f0dbb6c0394c7d1c0047aee016b8233
Sahril Angga Permana <43839821+sahrilvn@users.noreply.github.com>
2021-11-09 16:06:04 +0700

    Update +koneksi.php

--- +koneksi.php
+++ +koneksi.php
@@ -1,7 +1,5 @@
-<?php 
-// isi nama host, username mysql, dan password mysql anda
+<?php
 $host = mysql_connect("localhost", "root", "");
 
-// isikan dengan nama database yang akan di hubungkan
 $db = mysql_select_db("crud1");
 ?>
